d1d10a99044ff9bebba30d7e6e4ce157560de9d1
kazuopp312 <kazuopp312@gmail.com>
2021-03-23 01:58:34 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Amazon Store</h1>
+      {/* Header */}
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_u_tzBu5fnfsNP8XHuxy1MsvWwrfui_U",
+  authDomain: "clone-3bd64.firebaseapp.com",
+  projectId: "clone-3bd64",
+  storageBucket: "clone-3bd64.appspot.com",
+  messagingSenderId: "1066276557041",
+  appId: "1:1066276557041:web:d5f2905e2280c9e8415950",
+  measurementId: "G-XF30QD65TL"
+};
