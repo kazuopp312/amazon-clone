1276838cfdb6e83ef750598812583c43f2c3066d
kazuopp312 <kazuopp312@gmail.com>
2021-03-25 23:17:23 +0900

    problem with getBasketTotal function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Checkout from './Checkout';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="app">
-      <h1>Amazon Store</h1>
-      {/* Header */}
-      {/* Home */}
+      <Header />
+      <Switch>
+        <Route path ="/checkout">
+          <Checkout />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+        
+      </Switch>
     </div>
+    </Router>
   );
 }
 
